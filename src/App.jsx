c8722dec2d41e82ad282b1e0fb078f478ddbb433
Rory Goddard <goddardrory@gmail.com>
2025-02-04 18:32:41 +0200

    Fix: Remove irrelevant prop for NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -335,7 +335,6 @@ function App() {
     return (
         <>
             <NavBar
-                onFilterChange={handleFilterChange}
                 onFavoritesClick={handleFavoritesClick}
                 onResetClick={handleResetClick}
             />
