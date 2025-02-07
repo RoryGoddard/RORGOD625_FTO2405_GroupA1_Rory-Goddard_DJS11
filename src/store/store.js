84072648366fb74abb2ffebb8bea5041a4d6a520
Rory Goddard <goddardrory@gmail.com>
2025-02-07 21:13:08 +0200

    Feat: Add localstorage reducer and favourites middleware to store

--- src/store/store.js
+++ src/store/store.js
@@ -3,16 +3,19 @@ import { setupListeners } from '@reduxjs/toolkit/query'
 import { podcastApi } from '../services/podcastApi'
 import podcastReducer from '../state/podcastSlice'
 import navbarReducer from '../state/navbarSlice'
+import favouritesReducer from '../state/favouritesSlice'
+import localStorageMiddleware from '../middleware/localStorageMiddleware'
 
 export const store = configureStore({
   reducer: {
     podcasts: podcastReducer,
     [podcastApi.reducerPath]: podcastApi.reducer,
     navbar: navbarReducer,
+    favourites: favouritesReducer,
   },
 
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(podcastApi.middleware)
+    getDefaultMiddleware().concat(podcastApi.middleware, localStorageMiddleware)
   ,
 })
 
