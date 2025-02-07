d0d753f8cbf88fda2737d9271f12c7a9f4301d1d
Rory Goddard <goddardrory@gmail.com>
2025-02-07 21:17:02 +0200

    Fix: typo on "startsWith" function in localstorageMiddleware

--- src/middleware/localStorageMiddleware.js
+++ src/middleware/localStorageMiddleware.js
@@ -1,7 +1,7 @@
 const localStorageMiddleware = store => next => action => {
     const result = next(action)
 
-    if (action.type.startswith("/favourites")) {
+    if (action.type.startsWith("favourites/")) {
         const state = store.getState();
         localStorage.setItem("favourites", JSON.stringify(state.favourites));
     }
