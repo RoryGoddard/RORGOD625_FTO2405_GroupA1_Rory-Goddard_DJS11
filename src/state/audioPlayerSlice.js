553453b7c55e6f41a19838a6db6861b95c29ed10
Rory Goddard <goddardrory@gmail.com>
2025-02-07 22:58:11 +0200

    Feat: create audioPlayerSlice logic

--- src/state/audioPlayerSlice.js
+++ src/state/audioPlayerSlice.js
@@ -1,8 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-    currentEpisode: null,
-    isPlaying: false,
-    playingShow: null,
-    episodeTimestamps: null,
+const loadTimestamps = () => {
+    try {
+        const timestamps = localStorage.getItem("timestamps");
+        return timestamps ? JSON.parse(timestamps) : {};
+    } catch (error) {
+        console.error("Error loading timestamps from storage", error)
+        return {}
+    }
 }
+
+
+const audioPlayerSlice = createSlice({
+    name: "audioPlayer",
+    initialState: {
+        currentEpisode: null,
+        isPlaying: false,
+        playingShow: null,
+        volume: 50,
+        timestamps: loadTimestamps(),
+    },
+    reducers: {
+        setCurrentEpisode: (state, action) => {
+            state.currentEpisode = action.payload
+        },
+        setIsPlaying: (state, action) => {
+            state.isPlaying = action.payload
+        },
+        setVolume: (state, action) => {
+            state.volume = action.payload
+        },
+        saveTimestamp: (state, action) => {
+            const { episodeId, timestamp } = action.payload;
+            state.timestamps[episodeId] = timestamp;
+        }
+    }
+
+})
+
+export const { setCurrentEpisode, setPlaying, setVolume, saveTimestamp } = audioPlayerSlice.actions;
+export default audioPlayerSlice.reducer;
