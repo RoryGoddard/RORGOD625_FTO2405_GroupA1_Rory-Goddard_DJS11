1de21c09132f5786b24aca47f9dd97a239efbaa8
Rory Goddard <goddardrory@gmail.com>
2025-02-07 23:01:29 +0200

    Feat: Update localStorageMiddleware to include logic for timestamps

--- src/middleware/localStorageMiddleware.js
+++ src/middleware/localStorageMiddleware.js
@@ -6,6 +6,11 @@ const localStorageMiddleware = store => next => action => {
         localStorage.setItem("favourites", JSON.stringify(state.favourites));
     }
 
+    if (action.type.startsWith("audioPlayer/saveTimestamp")) {
+        const state = store.getState();
+        localStorage.setItem("timestamps", JSON.stringify(state.audioPlayer.timestamps))
+    }
+    
     return result;
 }
 
