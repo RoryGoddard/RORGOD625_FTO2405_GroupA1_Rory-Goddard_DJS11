e4035783309ece42d2149bb3c86e4f2c53f4c733
Rory Goddard <goddardrory@gmail.com>
2025-02-08 18:10:49 +0200

    Feat: First rough draft for episode component

--- src/components/PodcastDetailsModal.jsx
+++ src/components/PodcastDetailsModal.jsx
@@ -191,43 +191,7 @@ const PodcastDetailsModal = ({ show, open, onClose, onPlayEpisode, loading, fetc
                             </Box>
                             <Box sx={{ flexGrow: 1, overflow: 'auto' }}>
                                 <List>
-                                    {selectedSeason && selectedSeason.episodes.map((episode) => {                                        
-                                        return (
-                                        <ListItem key={episode.episode} divider>
-                                        <IconButton onClick={() => handleToggleFavourite(episode)}>
-                                            { isFavourite ? <FavoriteBorderIcon /> : <FavoriteIcon />}
-                                        </IconButton>
-                                        <ListItemText
-                                            primary={`Episode ${episode.episode}: ${episode.title}`}
-                                            secondary={
-                                            <>
-                                                {episode.description}
-                                                {timestamps[show.id] && timestamps[show.id][episode.title] && (
-                                                <Typography variant="caption" display="block">
-                                                    Last played: {formatTime(timestamps[show.id][episode.title])}
-                                                </Typography>
-                                                )}
-                                            </>
-                                            }
-                                        />
-                                        <ListItemSecondaryAction>
-                                            {/* {listenedEpisodes.indexOf({
-                                                                showId: show.id,
-                                                                showTitle: show.title,
-                                                                seasonTitle: selectedSeason.title,
-                                                                episodeTitle: episode.title,
-                                                                episodeNumber: episode.episode,
-                                                                updated: show.updated
-                                                                }) !== -1 && (
-                                            <CheckCircleIcon color="primary" sx={{ mr: 1 }} />
-                                            )} */}
-                                            <IconButton edge="end" aria-label="play" onClick={() => onPlayEpisode(episode)}>
-                                            <PlayArrowIcon />
-                                            </IconButton>
-                                        </ListItemSecondaryAction>
-                                        </ListItem>
-                                        
-                                    )})}
+                                   
                                 </List>
                             </Box>
                             <Button onClick={onClose} sx={{ mt: 2 }}>Close</Button>
