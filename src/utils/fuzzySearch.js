b933ad5eb2455b9f560f365df33707c0a5c68b3a
Rory Goddard <goddardrory@gmail.com>
2025-02-09 19:06:09 +0200

    Feat: Implement searching using redux - changed fuzzysearch to adapt keys depending on input

--- src/utils/fuzzySearch.js
+++ src/utils/fuzzySearch.js
@@ -1,14 +1,19 @@
 import Fuse from 'fuse.js';
 
+const favouriteKeys = ['showTitle', 'seasonTitle', 'episodeTitle', 'episodeNumber']
+const podcastKeys = ['title', 'description']
+
 export const initializeFuzzySearch = (shows) => {
   if (!shows || shows.length === 0) {
     return null;
   }
 
+  const isPodcastsOrFavourites = shows[0]?.title ? true : false
+
   const options = {
-    keys: ['title', 'description'],
+    keys: isPodcastsOrFavourites ? podcastKeys : favouriteKeys,
     includeScore: true,
-    threshold: 0.45
+    threshold: 0.3
   };
 
   return new Fuse(shows, options);
