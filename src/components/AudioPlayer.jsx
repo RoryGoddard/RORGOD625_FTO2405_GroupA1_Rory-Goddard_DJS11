645d84701b9663bdb6dc50cc0ca2800caea62f8d
Rory Goddard <goddardrory@gmail.com>
2025-02-18 12:56:01 +0200

    Fix: Remove unused generatePlaylist import from audioPlayer.jsx

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -6,7 +6,7 @@ import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import PropTypes from 'prop-types';
 import { useTheme } from '@mui/material/styles';
 import { useDispatch, useSelector } from 'react-redux';
-import { playEpisode, generatePlaylist, togglePlayPause, skipToNextEpisode, skipToPreviousEpisode, setCurrentTime } from '../state/audioPlayerSlice'
+import { playEpisode, togglePlayPause, skipToNextEpisode, skipToPreviousEpisode, setCurrentTime } from '../state/audioPlayerSlice'
 import Volume from './Volume'
 import AudioSlider from './AudioSlider';
 
