c6820bd784853de103ec0fb068fa8cd0ca92e0b4
Rory Goddard <goddardrory@gmail.com>
2025-02-20 23:23:57 +0200

    Fix: Sorting only working off updated key for favourites, and refactored switch cases to be more concise

--- src/utils/sortUtils.js
+++ src/utils/sortUtils.js
@@ -1,47 +1,28 @@
-// Functions to handle sorting for shows and favorite episodes
-
-export const sortByTitleAscending = (data, titleKey = 'title') => {
-  return [...data].sort((a, b) => {
-    const titleA = a[titleKey] || a.showTitle || '';
-    const titleB = b[titleKey] || b.showTitle || '';
-    return titleA.localeCompare(titleB);
-  });
-};
-
-export const sortByTitleDescending = (data, titleKey = 'title') => {
-  return [...data].sort((a, b) => {
-    const titleA = a[titleKey] || a.showTitle || '';
-    const titleB = b[titleKey] || b.showTitle || '';
-    return titleB.localeCompare(titleA);
-  });
-};
-
-export const sortByDateAscending = (data, dateKey = 'updated') => {
-  return [...data].sort((a, b) => {
-    const dateA = new Date(a[dateKey] || a.savedAt || 0);
-    const dateB = new Date(b[dateKey] || b.savedAt || 0);
-    return dateA - dateB;
-  });
-};
-
-export const sortByDateDescending = (data, dateKey = 'updated') => {
-  return [...data].sort((a, b) => {
-    const dateA = new Date(a[dateKey] || a.savedAt || 0);
-    const dateB = new Date(b[dateKey] || b.savedAt || 0);
-    return dateB - dateA;
-  });
-};
-
 export const applySorting = (podcasts, sortOption) => {
+  if (podcasts[0]?.episodeId) {
+    switch (sortOption) {
+      case 'A-Z':
+        return [...podcasts].sort((a, b) => a.showTitle.localeCompare(b.showTitle));
+      case 'Z-A':
+        return [...podcasts].sort((a, b) => b.showTitle.localeCompare(a.showTitle));
+      case 'newest':
+        return [...podcasts].sort((a, b) => new Date(b.savedAt) - new Date(a.savedAt));
+      case 'oldest':
+        return [...podcasts].sort((a, b) => new Date(a.savedAt) - new Date(b.savedAt));
+      default:
+        return podcasts;
+    }
+  }
+  
   switch (sortOption) {
     case 'A-Z':
-      return sortByTitleAscending(podcasts);
+      return [...podcasts].sort((a, b) => a.title.localeCompare(b.title));
     case 'Z-A':
-      return sortByTitleDescending(podcasts);
+      return [...podcasts].sort((a, b) => b.title.localeCompare(a.title));
     case 'newest':
-      return sortByDateDescending(podcasts);
+      return [...podcasts].sort((a, b) => new Date(b.updated) - new Date(a.updated));
     case 'oldest':
-      return sortByDateAscending(podcasts);
+      return [...podcasts].sort((a, b) => new Date(a.updated) - new Date(b.updated));
     default:
       return podcasts;
   }
