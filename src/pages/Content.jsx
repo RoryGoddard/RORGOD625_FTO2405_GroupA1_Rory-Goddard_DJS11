b84218e256fa0602862ede4e3850be5fa72f7de1
Rory Goddard <goddardrory@gmail.com>
2025-02-21 13:01:47 +0200

    Fix: Adjust margin for content grid so show cards are fully visible at the bottom of the page

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -37,8 +37,10 @@ function Content() {
             />
             <Grid2 container spacing={{ xs: 2, md: 3 }} 
             sx={{ 
-                margin:"1.5rem",
-                marginTop:"5rem"
+                marginTop:"5rem",
+                marginBottom: "10rem",
+                marginLeft: "1.5rem",
+                marginRight: "1.5rem"
             }}>
             {sortedPodcasts.map((podcast) => (
                 <Grid2 key={podcast.id} size={{ xs: 12, sm: 6, md: 6, lg:4, xl:3 }} sx={{borderRadius: "2%"}}>
