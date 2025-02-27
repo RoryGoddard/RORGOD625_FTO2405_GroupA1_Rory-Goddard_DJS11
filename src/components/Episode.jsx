bc10d09b03939377492aee7f581aced657134229
Rory Goddard <goddardrory@gmail.com>
2025-02-27 20:49:28 +0200

    Fix: add math.floor to remaining seconds as well

--- src/components/Episode.jsx
+++ src/components/Episode.jsx
@@ -35,7 +35,7 @@ const Episode = ({ episode }) => {
 
     const formatTime = (seconds) => {
         const minutes = Math.floor(seconds / 60);
-        const remainingSeconds = seconds % 60;
+        const remainingSeconds = Math.floor(seconds % 60)
         return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
     };    
 
